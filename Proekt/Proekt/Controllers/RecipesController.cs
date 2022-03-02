8d24a1cb646330dd53a3579aca46b75af36571f2
Sandra Nikoloska <sandranikoloska999@gmail.com>
2022-03-02 18:33:26 +0100

    version 1

---
+++ Proekt/Proekt/Controllers/RecipesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proekt.Models;
+
+namespace Proekt.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Recipes
+        public ActionResult Index()
+        {
+            return View(db.Recipes.ToList());
+        }
+
+        // GET: Recipes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipes recipes = db.Recipes.Find(id);
+            if (recipes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipes);
+        }
+
+        // GET: Recipes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Image,Ingredients,Description")] Recipes recipes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Recipes.Add(recipes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recipes);
+        }
+
+        // GET: Recipes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipes recipes = db.Recipes.Find(id);
+            if (recipes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipes);
+        }
+
+        // POST: Recipes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Image,Ingredients,Description")] Recipes recipes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recipes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recipes);
+        }
+
+        // GET: Recipes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipes recipes = db.Recipes.Find(id);
+            if (recipes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipes);
+        }
+
+        // POST: Recipes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Recipes recipes = db.Recipes.Find(id);
+            db.Recipes.Remove(recipes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proekt/Proekt/Models/Recipes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proekt.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Ingredients { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
